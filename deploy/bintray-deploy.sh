4a822b0b8c11b190fa9de3adfcfd5f11e8c2aefa
Rafael Ostertag <rafi@guengel.ch>
2018-04-22 09:22:57 +0200

    Added script creating bintray-deploy.json.

---
+++ deploy/bintray-deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Create bintray-deploy.json as expected by .travis.yml.
+#
+# Call this script from the repository root
+
+TRAVIS_TAG=${TRAVIS_TAG:-tag expected}
+
+if ! echo "${TRAVIS_TAG}" | grep -E "^v[0-9]+\.[0-9]+\.[0-9]+\$" >/dev/null 2>&1
+then
+    echo 'Tag does not match "^v[0-9]+.[0-9]+.[0-9]+$". Assuming non-release tag and doing nothing.'
+    # Don't make the job fail. Maybe it's a legit non-release tag
+    exit 0
+fi
+
+SEMANTIC_VERSION=${TRAVIS_TAG#v}
+
+cat > bintray-deploy.json <<EOF
+{
+    "package": {
+        "name": "auto-upload",
+        "repo": "anathpki/anath-buildtools",
+        "desc": "I was pushed completely automatically",
+        "website_url": "https://github.com/AnathPKI/anath-buildtools",
+        "vcs_url": "https://github.com/AnathPKI/anath-buildtools.git",
+        "licenses": ["BSD-2-CLAUSE"]
+    },
+    "version": {
+        "name": "${SEMANTIC_VERSION}",
+        "vcs_tag": "${TRAVIS_TAG}",
+        "gpgSign": false
+    },
+
+    "files":
+        [
+        {"includePattern": "restart-build|trigger-build", uploadPattern": "${TRAVIS_TAG}/"}
+        ],
+    "publish": true
+}
+EOF
